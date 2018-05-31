d950a6b681e2d70640ec2f898f68ca7585d1823e
trinhduc <trinhduc.thd@gmail.com>
2018-05-31 20:05:05 +0700

    make macro multiple line with \ at end line

--- string/owner-strlen.cpp
+++ string/owner-strlen.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
-#define SAY_HI() cout << "Rat vui khi gap lai ban" << endl;
+#define SAY_HI() cout << endl; \
+				 cout << "Rat vui khi gap lai ban" << endl; \
+				 cout << "Mot ngay san khoai dang den voi ban" << endl; \
+				 cout << "Toi co the thay nu cuoi cua ban.....^_^" << endl; \
+				 cout << "Waooo... nu cuoi ban rat dep...!" << endl; \
+				 cout << endl;	
 
 using namespace std;
 
