430d291f4b112cb24c8a236740de82c97b003427
trinhduc <trinhduc.thd@gmail.com>
2018-05-31 21:01:55 +0700

    Split program into 2 files & compiler with file .h  external over -lm at the end command line

--- string/owner-strlen.cpp
+++ string/owner-strlen.cpp
@@ -1,15 +1,7 @@
 #include <iostream>
-
-#define SAY_HI() cout << endl; \
-				 cout << "Rat vui khi gap lai ban" << endl; \
-				 cout << "Mot ngay san khoai dang den voi ban" << endl; \
-				 cout << "Toi co the thay nu cuoi cua ban.....^_^" << endl; \
-				 cout << "Waooo... nu cuoi ban rat dep...!" << endl; \
-				 cout << endl;	
+#include "owner-strlen.h"
 
 using namespace std;
-
-long do_dai_chuoi(char *chuoi);
 
 int main (int argc , char *argv[])
 {
---
+++ string/owner-strlen.h
@@ -0,0 +1,9 @@
+
+#define SAY_HI() cout << endl; \
+				 cout << "Rat vui khi gap lai ban" << endl; \
+				 cout << "Mot ngay san khoai dang den voi ban" << endl; \
+				 cout << "Toi co the thay nu cuoi cua ban.....^_^" << endl; \
+				 cout << "Waooo... nu cuoi ban rat dep...!" << endl; \
+				 cout << endl;	
+
+long do_dai_chuoi(char *chuoi);
